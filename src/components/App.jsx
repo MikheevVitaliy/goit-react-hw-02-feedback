3de362559fdc88614e63f43e40ac103d2787cb31
MikheevVitaliy <mikheev.vitaliy87@gmail.com>
2023-04-20 14:02:25 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -70,3 +70,59 @@ export class App extends Component {
     );
   }
 }
+
+// =================
+export const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const feedback = { good, neutral, bad };
+
+  const handleBtnClick = ({ target: { value } }) => {
+    switch (value) {
+      case 'good':
+        return setGood(s => s + 1);
+      case 'neutral':
+        return setNeutral(s => s + 1);
+      case 'bad':
+        return setBad(s => s + 1);
+      default:
+        return;
+    }
+  };
+
+  const countTotalFeedback = () =>
+    Object.values(feedback).reduce((total, prevState) => total + prevState, 0);
+
+  const countPositiveFeedbackPercentage = () =>
+    Math.round((good / countTotalFeedback()) * 100);
+
+  return (
+    <Container>
+      <Section title="Please leave feedback">
+        <FeedbackOptions
+          options={Object.keys(feedback)}
+          onLeaveFeedback={handleBtnClick}
+        ></FeedbackOptions>
+      </Section>
+
+      <Section title="Statistics">
+        {countTotalFeedback() > 0 ? (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={countTotalFeedback}
+            positivePercentage={countPositiveFeedbackPercentage}
+          />
+        ) : (
+          <Notification message="There is no feedback" />
+        )}
+      </Section>
+
+      <GlobalStyle />
+    </Container>
+  );
+};
+// =========================
